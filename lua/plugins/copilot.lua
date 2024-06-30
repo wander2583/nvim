86b2bb30191d45329b74bad2269fac1b1eea6bcb
Wander <wanderlei2583@gmail.com>
2024-06-29 21:39:59 -0300

    criando configurações para o copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,22 @@
+return {
+  -- {
+  --   "zbirenbaum/copilot.nvim",
+  --   event = { "BufEnter" },
+  --   config = function()
+  --     require("copilot").setup({
+  --       suggestion = {
+  --         enabled = false,
+  --       },
+  --       panel = { enabled = false },
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   event = { "BufEnter" },
+  --   dependencies = { "zbirenbaum/copilot.nvim" },
+  --   config = function()
+  --     require("copilot-cmp").setup()
+  --   end,
+  -- },
+}
